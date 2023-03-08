4ab5152e58cfacf9b29bc8fbc7aa9737466f51e5
Pholosho Seloane <35956639+pholoshos@users.noreply.github.com>
2023-03-08 19:17:31 +0200

    push

--- pages/radio/index.tsx
+++ pages/radio/index.tsx
@@ -1,116 +1,137 @@
-import { resolveObjectURL } from 'buffer';
-import type { NextPage } from 'next'
-import { useEffect, useRef, useState } from 'react'
-import { Alert, Button, Container, Form } from 'react-bootstrap'
-import socket from '../../util/socket'
-import { getMedia } from '../../util/webrtc'
-
+import { resolveObjectURL } from "buffer";
+import type { NextPage } from "next";
+import { useEffect, useRef, useState } from "react";
+import { Alert, Button, Container, Form } from "react-bootstrap";
+import socket from "../../util/socket";
+import { getMedia } from "../../util/webrtc";
 
 interface IMessage {
-    username?: string;
-    text?: string;
-    time?: string;
+  username?: string;
+  text?: string;
+  time?: string;
 }
 interface IUserData {
-    final?: {
-        username?: string;
-    },
-    pre?: {
-        username?: string;
-    },
+  final?: {
+    username?: string;
+  };
+  pre?: {
+    username?: string;
+  };
 }
 
 const Home: NextPage = () => {
-    const [messages, setMessages] = useState<IMessage[]>([]);
-    const [message, setMessage] = useState<string>('Hello');
-    const [recieved, setRecieved] = useState<IMessage>({});
-    const [userDetails, setDetails] = useState<IUserData>({});
-    let myRef = useRef<HTMLInputElement>(null)
-    const [image, setImage] = useState('')
-    const [audio, setAudio] = useState('')
+  const [messages, setMessages] = useState<IMessage[]>([]);
+  const [message, setMessage] = useState<string>("Hello");
+  const [recieved, setRecieved] = useState<IMessage>({});
+  const [user, setUser] = useState({ host: "", key: "" });
+  const [userDetails, setDetails] = useState<IUserData>({});
+  let myRef = useRef<HTMLInputElement>(null);
+  const [image, setImage] = useState("");
+  const [audio, setAudio] = useState("");
+  const hosts = [
+    { host: "pholosho", key: "l0vesod@" },
+    { host: "other", key: "l0vesod@" },
+  ];
 
-    const [isRecording,setIsRecording] = useState(false);
-    //console.log("LOG::: ref ",myRef?.current?.value);
+  const [isAuthed, setIsAuthed] = useState(false);
 
-    const record = () => {
-        getMedia()
+  const [isRecording, setIsRecording] = useState(false);
+  //console.log("LOG::: ref ",myRef?.current?.value);
+
+  const record = () => {
+    getMedia();
+  };
+
+  const onLogin = () => {
+    const isTrue = !!hosts.find(
+      (host) => host.host === user.host && host.key === user.key
+    );
+    setIsAuthed(isTrue);
+  };
+
+  const handleSendMessage = () => {
+    const data = {
+      text: " " + message,
+      username: userDetails?.final?.username,
+      time: Date.now().toString(),
+    };
+    setMessages([...messages, { ...data, username: "you" }]);
+    socket.emit("sendMessage", data);
+  };
+
+  useEffect(() => {
+    const username = user.host;
+    socket.auth = { username };
+    socket.connect();
+    socket.connected ? console.log("connected") : console.log("not connected!");
+  });
+
+  useEffect(() => {
+    socket.on("hasMessage", (args: any) => {
+      setRecieved(args);
+    });
+    socket.on("recieveImage", (args: any) => {
+      const blob = new Blob([args.image]);
+      const srcBlob = URL.createObjectURL(blob);
+      setImage(srcBlob);
+    });
+  }, []);
+
+  useEffect(() => {
+    const txts = messages.map((m: any) => m?.text);
+    if (!txts.includes(recieved)) {
+      setMessages([...messages, recieved]);
+      //console.log("LOG:::current Messages", messages)
     }
-    const handleSendMessage = () => {
-
-        const data = {
-            text: " " + message,
-            username: userDetails?.final?.username,
-            time: Date.now().toString()
-        }
-        setMessages([...messages, { ...data, username: 'you' }])
-        socket.emit('sendMessage', data)
-    }
-
-    useEffect(() => {
-        const username = "pholosho";
-        socket.auth = { username };
-        socket.connect()
-        socket.connected ? console.log("connected") : console.log("not connected!")
-
-    })
-
-
-    useEffect(() => {
-
-        socket.on("hasMessage", (args: any) => {
-            setRecieved(args);
-        })
-        socket.on("recieveImage", (args : any) => {
-
-            const blob = new Blob([args.image])
-            const srcBlob = URL.createObjectURL(blob);
-            setImage(srcBlob)
-
-        })
-
-        socket.on("recieveAudio", (args :any) => {
-
-            const blob = new Blob([args.audio])
-            const srcBlob = URL.createObjectURL(blob);
-
-            setAudio(srcBlob);
-
-        })
-
-    }, [])
-
-    useEffect(() => {
-        const txts = messages.map((m: any) => m?.text);
-        if (!txts.includes(recieved)) {
-            setMessages([...messages, recieved])
-            //console.log("LOG:::current Messages", messages)
-        }
-
-    }, [recieved])
-    return (
+  }, [recieved]);
+  return (
+    <>
+      {(isAuthed && (
         <>
-            <h1>SodaHouse</h1>
-            <Alert variant={isRecording? 'success': 'warning'} >
-                {isRecording? 'on air!': 'offline'}
-            </Alert>
+          <h1>SodaHouse</h1>
+          <Alert variant={isRecording ? "success" : "warning"}>
+            {isRecording ? "on air!" : "offline"}
+          </Alert>
 
-            <Button disabled={isRecording} onClick={() => {
-                setIsRecording(true)
-                record()
+          <Button
+            disabled={isRecording}
+            onClick={() => {
+              setIsRecording(true);
+              record();
+            }}
+          >
+            Start Broadcasting .
+          </Button>
 
-            }}>Start Broadcasting .</Button>
-
-            <Button onClick={() => {
-                setIsRecording(false)
-
-            }}>Stop Broadcasting .</Button>
-            <audio autoPlay src={audio}></audio>
-
-
-
+          <Button
+            onClick={() => {
+              setIsRecording(false);
+            }}
+          >
+            Stop Broadcasting .
+          </Button>
+          <audio autoPlay src={audio}></audio>
+        
         </>
+      )) || (
+        <>
+          <br></br>
+          <h1>Welcome to sodahouse</h1>
+          <p>Enter details below</p>
 
-    )
-}
+          <Form>
+            <Form.Label>Host name</Form.Label>
+            <Form.Control onChange={({target})=>setUser({...user,host:target.value})} placeholder="host name here"></Form.Control>
 
-export default Home
+            <Form.Label>Radio Key</Form.Label>
+            <Form.Control onChange={({target})=>setUser({...user,key:target.value})}  placeholder="Enter key"></Form.Control>
+            <br></br>
+            <Button onClick={onLogin} variant="outline-success">Lets go</Button>
+          </Form>
+        </>
+      )}
+    </>
+  );
+};
+
+export default Home;
